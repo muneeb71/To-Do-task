c6e5be6e25bd2a736a0a28827c50e3edfe334394
muneeb <muneeb@muneebs-MacBook-Pro.local>
2023-06-05 19:37:43 +0500

    final-commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,126 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCheck } from "@fortawesome/free-solid-svg-icons";
+import { faEllipsisH } from "@fortawesome/free-solid-svg-icons";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
 function App() {
+  const [tasks, setTasks] = useState();
+  const [input, setInput] = useState("");
+  useEffect(() => {
+    getTasks();
+  }, []);
+  const addItem = async () => {
+    if(input.length == 0){
+      alert('Please Enter Your Task')
+      return;
+    }
+    try {
+      const res = await axios.post("http://localhost:4000/add-task", {
+        task: input,
+      });
+      setTasks([...tasks, res.data]);
+      setInput("");
+     
+    } catch (error) {
+      console.log(`error -> ${error}`);
+    }
+  };
+  const getTasks = async () => {
+    try {
+      const new_tasks = await axios.get("http://localhost:4000/get-tasks");
+      setTasks(new_tasks.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+  const handleInputChange = (event) => {
+    setInput(event.target.value);
+  };
+  const handleKeyPress = (event) => {
+    if (event.key === "Enter") {
+      addItem();
+    }
+  }
+  const handleDelete = async(_id) => {
+    try {
+      const updated_tasks = await axios.delete("http://localhost:4000/delete-task/" + _id);
+     
+      const updated_array = tasks.filter((task) => task._id != _id);
+      setTasks(updated_array)
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="input">
+        <div>
+          <svg
+            className="icon_bar"
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 448 512"
+          >
+            <path d="M0 96C0 78.3 14.3 64 32 64H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32C14.3 128 0 113.7 0 96zM0 256c0-17.7 14.3-32 32-32H416c17.7 0 32 14.3 32 32s-14.3 32-32 32H32c-17.7 0-32-14.3-32-32zM448 416c0 17.7-14.3 32-32 32H32c-17.7 0-32-14.3-32-32s14.3-32 32-32H416c17.7 0 32 14.3 32 32z" />
+          </svg>
+
+          <input
+            className="inp_field"
+            name="task"
+            value={input}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyPress}
+          ></input>
+          <svg
+            onClick={addItem}
+            
+            className="icon_arrow"
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 448 512"
+          >
+            <path d="M256 80c0-17.7-14.3-32-32-32s-32 14.3-32 32V224H48c-17.7 0-32 14.3-32 32s14.3 32 32 32H192V432c0 17.7 14.3 32 32 32s32-14.3 32-32V288H400c17.7 0 32-14.3 32-32s-14.3-32-32-32H256V80z" />
+          </svg>
+        </div>
+        <div style={{ marginTop: 20 }}>
+          {tasks &&
+            tasks.map((task, index) => (
+              <div className="items" key={index}>
+                <FontAwesomeIcon
+                  icon={faCheck}
+                  style={{
+                    backgroundColor: "#a8662c",
+                    color: "white",
+                    paddingLeft: 8,
+                    paddingRight: 8,
+                    paddingTop: 5,
+                    paddingBottom: 5,
+                    borderRadius: 40,
+                    marginTop: 16,
+                  }}
+                />
+                <h4>{task.task}</h4>
+                <FontAwesomeIcon
+                  icon={faEllipsisH}
+                  style={{
+                    backgroundColor: "#a8662c",
+                    color: "white",
+                    paddingLeft: 8,
+                    paddingRight: 8,
+                    paddingTop: 5,
+                    paddingBottom: 5,
+                    borderRadius: 40,
+                    marginTop: 16,
+                  }}
+                  onClick={() => {handleDelete(task._id)}}
+                />
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
